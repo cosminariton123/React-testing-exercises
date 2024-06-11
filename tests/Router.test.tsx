8fabdcd24a6a20e229d9079f66ed17dbf66aac93
cosminariton123 <cosmin.ariton@gmail.com>
2024-06-11 20:42:05 +0300

    Testing Routes

---
+++ tests/Router.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import routes from "../src/routes";
+
+describe("Router", () => {
+    it("should render the home page for /", () => {
+        const router = createMemoryRouter(routes, {
+            initialEntries: ["/"],
+        });
+
+        render(<RouterProvider router={router}></RouterProvider>);
+
+        expect(
+            screen.getByRole("heading", { name: /home/i })
+        ).toBeInTheDocument();
+    });
+
+    it("should render the products page for /products", () => {
+        const router = createMemoryRouter(routes, {
+            initialEntries: ["/products"],
+        });
+
+        render(<RouterProvider router={router}></RouterProvider>);
+
+        expect(
+            screen.getByRole("heading", { name: /products/i })
+        ).toBeInTheDocument();
+    });
+});
